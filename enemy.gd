7ad52d7ef5fad894c9ca9469c89b857ba9b8675f
6ust <gust.guedes95@gmail.com>
2021-10-04 17:42:22 -0300

    Timer animação de morte inimigo

--- enemy.gd
+++ enemy.gd
@@ -6,24 +6,32 @@ var velocity = 600
 var start
 var end
 var lifeEnemyZombie = randi()%14+8
+var timer = Timer.new()
 
 func _ready():
 	start = position.x
 	end = start + velocity
 
 # ALGORITMO DE MOVIMENTAÇÃO DO INIMIGO ZOMBIE
-func _process(delta):
+func _physics_process(delta):
+	
+	# GRAVIDADE APLICADA AO INIMIGO
 	if position.x < end and direction == "right":
+		get_node("AnimatedSprite").play("walking")
 		get_node("AnimatedSprite").set_flip_h(false)
-		position.x += 2
+		if lifeEnemyZombie > 0:
+			position.x += 2
 	else: 
 		direction = "left"
 	
 	if position.x > start and direction == "left":
+		get_node("AnimatedSprite").play("walking")
 		get_node("AnimatedSprite").set_flip_h(true)
-		position.x -= 2
+		if lifeEnemyZombie > 0:
+			position.x -= 2
 	else:
 		direction = "right"
+		
 		
 # VIDA DO INIMIGO 1 - ZOMBIE MORTE ALEATORIA DE 8 A 14 HITS
 func lifeEnemy(lifeE1):
@@ -34,6 +42,14 @@ func lifeEnemy(lifeE1):
 # ANIMAÇÃO DE MORTE DO INIMIGO ZOMBIE
 func die():
 	get_node("AnimatedSprite").play("dying")
-	#IMPLEMENTAR PARADA DE MOVIMENTO QUANDO MORRER
-	#IMPLEMENTAR TIMER PARA DESAPARENCIMENTO
-	queue_free()		
+	
+	#TIMER PARA DESAPARENCIMENTO
+	timer.set_wait_time(2)
+	timer.set_one_shot(true)
+	self.add_child(timer)
+	timer.start()
+	yield(timer, "timeout")
+	timer.queue_free()
+	
+	# REMOÇÃO DO INIMIGO EM CENA
+	queue_free()
